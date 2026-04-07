ec24d360444c33611e8328a5b8ca80a1c8b4a377
Reda-Kriate <kriatereda@gmail.com>
2026-04-08 00:16:51 +0100

    commit

---
+++ src/main/java/org/reda/mcpjavapythonclientserver/McpJavaPythonClientServerApplication.java
@@ -0,0 +1,13 @@
+package org.reda.mcpjavapythonclientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpJavaPythonClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpJavaPythonClientServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/reda/mcpjavapythonclientserver/McpJavaPythonClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reda.mcpjavapythonclientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpJavaPythonClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
