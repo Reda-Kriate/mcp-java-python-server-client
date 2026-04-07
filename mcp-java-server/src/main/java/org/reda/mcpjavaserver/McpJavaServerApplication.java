b2ad6fee346ec11c39cb8c583dc2eeb65ac36c28
Reda-Kriate <kriatereda@gmail.com>
2026-04-08 00:19:41 +0100

    comm

---
+++ mcp-java-server/src/main/java/org/reda/mcpjavaserver/McpJavaServerApplication.java
@@ -0,0 +1,13 @@
+package org.reda.mcpjavaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpJavaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpJavaServerApplication.class, args);
+    }
+
+}
---
+++ mcp-java-server/src/test/java/org/reda/mcpjavaserver/McpJavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reda.mcpjavaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpJavaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
